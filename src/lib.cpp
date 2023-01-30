4dc6c551c73a04c68371a9569740f1b0d4cda0bd
Mayukha Vadari <mvadari@ripple.com>
2023-01-30 18:38:59 -0500

    build .so

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,7 +1,7 @@
-#include <Python.h>
-#include <boost/python.hpp>
-#include <ripple/app/tx/impl/Transactor.h>
-#include <ripple/protocol/STTx.h>
+// #include <Python.h>
+// #include <boost/python.hpp>
+// #include <ripple/app/tx/impl/Transactor.h>
+// #include <ripple/protocol/STTx.h>
 
 /*
 Application& app;
@@ -12,11 +12,46 @@ beast::Journal const j;
 */
 
 
+// using namespace boost::python;
+
+// BOOST_PYTHON_MODULE(pylib)
+// {
+//     class_<ripple::STTx>("STTx", no_init)
+//       .def("getTransactionID", &ripple::STTx::getTransactionID)
+//     ;
+// }
+
+#include <boost/python.hpp>
+#include <boost/python/list.hpp>
+#include <boost/python/extract.hpp>
+#include <string>
+#include <sstream>
+#include <vector>
+
+struct World
+{
+    void set(std::string msg) { mMsg = msg; }
+    void many(boost::python::list msgs) {
+        long l = len(msgs);
+        std::stringstream ss;
+        for (long i = 0; i<l; ++i) {
+            if (i>0) ss << ", ";
+            std::string s = boost::python::extract<std::string>(msgs[i]);
+            ss << s;
+        }
+        mMsg = ss.str();
+    }
+    std::string greet() { return mMsg; }
+    std::string mMsg;
+};
+
 using namespace boost::python;
 
-BOOST_PYTHON_MODULE(pylib)
+BOOST_PYTHON_MODULE(classes)
 {
-    class_<ripple::STTx>("STTx", no_init)
-      .def("getTransactionID", &ripple::STTx::getTransactionID)
+    class_<World>("World")
+        .def("greet", &World::greet)
+        .def("set", &World::set)
+        .def("many", &World::many)
     ;
-}
+};
