0a27f8748e0d874ee032e2bb9a007519f25826c6
Mayukha Vadari <mvadari@ripple.com>
2023-01-31 10:45:13 -0500

    get test file working

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-import build.plugin_lib
+from build.plugin_lib import World
+
+t = World()
+t.set("bom dia!")
+print(t.greet())
+
+t.many(['Good Morning', 'Buon giorno', 'Kali mera'])
+print(t.greet())
