b3a7bb89d558b94fbf57ea8120bce0415c27f1d8
Mayukha Vadari <mvadari@ripple.com>
2023-01-31 18:15:14 -0500

    switch back to rippled code

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,11 @@
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
-
 namespace py = pybind11;
 
 PYBIND11_MODULE(plugin_transactor, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-        .. currentmodule:: cmake_example
-        .. autosummary::
-           :toctree: _generate
-           add
-           subtract
-    )pbdoc";
+    py::class_<ripple::STTx> STTx(m, "STTx");
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-        Some other explanation about the subtract function.
-    )pbdoc");
-
-#ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-    m.attr("__version__") = "dev";
-#endif
+    STTx
+        .def("getTransactionID", &ripple::STTx::getTransactionID);
 }
