da3ac64eb93000a37ee773a10fc2ace622b769c7
Mayukha Vadari <mvadari@ripple.com>
2023-02-01 15:12:45 -0500

    get barebones plugin transactor working

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import plugin_transactor
+from plugin_transactor import STTx, getAccount
 
-print(dir(plugin_transactor.STTx))
+
+def preflight(tx):
+    print("HIIIIIIIIII")
+    print(tx)
+    print(dir(tx))
+    print(dir(plugin_transactor))
+    print(tx.getTxnType())
+    print("DANGER ZONE")
+    print(getAccount(tx))
