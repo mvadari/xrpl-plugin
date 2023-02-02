3821c057607ba36e82dbc8d006d74290bb7a36fb
Mayukha Vadari <mvadari@ripple.com>
2023-02-01 23:57:42 -0500

    get STTx.at building

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
-#include <vector>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <ripple/app/tx/impl/Transactor.h>
+#include <pybind11/operators.h>
+
 #include <ripple/protocol/STTx.h>
 #include <ripple/protocol/TxFormats.h>
+#include <ripple/protocol/STAccount.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -18,12 +19,9 @@ PYBIND11_MODULE(plugin_transactor, m) {
         .value("ttACCOUNT_SET", ripple::TxType::ttACCOUNT_SET)
         .value("ttESCROW_CANCEL", ripple::TxType::ttESCROW_CANCEL)
         .value("ttREGULAR_KEY_SET", ripple::TxType::ttREGULAR_KEY_SET)
-        .value("ttNICKNAME_SET", ripple::TxType::ttNICKNAME_SET)
         .value("ttOFFER_CREATE", ripple::TxType::ttOFFER_CREATE)
         .value("ttOFFER_CANCEL", ripple::TxType::ttOFFER_CANCEL)
-        .value("ttCONTRACT", ripple::TxType::ttCONTRACT)
         .value("ttTICKET_CREATE", ripple::TxType::ttTICKET_CREATE)
-        .value("ttSPINAL_TAP", ripple::TxType::ttSPINAL_TAP)
         .value("ttSIGNER_LIST_SET", ripple::TxType::ttSIGNER_LIST_SET)
         .value("ttPAYCHAN_CREATE", ripple::TxType::ttPAYCHAN_CREATE)
         .value("ttPAYCHAN_FUND", ripple::TxType::ttPAYCHAN_FUND)
@@ -46,7 +44,65 @@ PYBIND11_MODULE(plugin_transactor, m) {
         .value("ttUNL_MODIFY", ripple::TxType::ttUNL_MODIFY)
         .export_values();
 
-    py::class_<ripple::STTx> STTx(m, "STTx");
+    py::class_<ripple::SField> SField(m, "SField");
+
+    py::class_<ripple::STAccount> STAccount(m, "STAccount");
+    // STAccount
+    //     .def(py::init<>());
+    
+    // py::class_<
+    //     ripple::TypedField<ripple::STAccount>,
+    //     std::shared_ptr< ripple::TypedField<ripple::STAccount> >,
+    //     ripple::SField
+    // >(m, "TypedFieldSTAccount");
+    //  py::class_<
+    //     ripple::TypedField<ripple::STBlob>,
+    //     std::shared_ptr< ripple::TypedField<ripple::STBlob> >,
+    //     ripple::SField
+    // >(m, "TypedFieldSTBlob");
+
+    // py::class_<
+    //     ripple::OptionaledField<ripple::STAccount>,
+    //     std::shared_ptr< ripple::OptionaledField<ripple::STAccount> >,
+    //     ripple::SField
+    // >(m, "OptionaledFieldSTAccount");
+    //  py::class_<
+    //     ripple::OptionaledField<ripple::STBlob>,
+    //     std::shared_ptr< ripple::OptionaledField<ripple::STBlob> >,
+    //     ripple::SField
+    // >(m, "OptionaledFieldSTBlob");
+
+    py::class_<ripple::STObject> STObject(m, "STObject");
+    STObject
+        .def(
+            "at",
+            (
+                typename ripple::STAccount::value_type // return type
+                (ripple::STObject::*) // class pointer?
+                (const ripple::TypedField<ripple::STAccount> &) // param type
+                const // const
+            )
+            &ripple::STObject::at, // function name
+            py::arg("f") // arg name
+        );
+
+    py::class_<ripple::STTx, ripple::STObject> STTx(m, "STTx");
     STTx
         .def("getTxnType", &ripple::STTx::getTxnType);
+    
+    // template<typename T>
+    // void declare_at(std::string &typestr) {
+    //     using TF = ripple::TypedField<T>;
+    //     std::string pyclass_name = std::string("TypedField") + typestr;
+    //     STTx
+    //         .def(
+    //             "at",
+    //             py::overload_cast<TF>(&ripple::STTx::at)
+    //         )
+    // }
+
+    // declare_at<ripple::STAccount>("STAccount");
+    // declare_at<ripple::STBlob>("STBlob");
+    
+//     m.attr("sfAccount") = ripple::sfAccount;
 }
