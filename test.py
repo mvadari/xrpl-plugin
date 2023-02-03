f2038fe22efc0e8def80e8420119d9673d7d51fe
Mayukha Vadari <mvadari@ripple.com>
2023-02-03 09:28:56 -0500

    get tx.Account working

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-from plugin_transactor import STTx
+import plugin_transactor
+from plugin_transactor import STTx, getAccount
 
 
 def preflight(tx):
+    print("HIIIIIIIIII")
     print(tx)
     print(dir(tx))
+    print(dir(plugin_transactor))
     print(tx.getTxnType())
+    print("DANGER ZONE")
+    print(getAccount(tx))
