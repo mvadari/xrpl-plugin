cbe23fc422f96992eda840a076d94d9136aa9acb
Mayukha Vadari <mvadari@ripple.com>
2023-02-03 09:28:56 -0500

    get tx.Account working

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 #include <pybind11/stl.h>
 #include <pybind11/operators.h>
 
-#include <ripple/protocol/STTx.h>
-#include <ripple/protocol/TxFormats.h>
-#include <ripple/protocol/STAccount.h>
+#include <ripple/app/tx/impl/Transactor.h>
+#include <ripple/protocol/st.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -47,62 +46,29 @@ PYBIND11_MODULE(plugin_transactor, m) {
     py::class_<ripple::SField> SField(m, "SField");
 
     py::class_<ripple::STAccount> STAccount(m, "STAccount");
-    // STAccount
-    //     .def(py::init<>());
-    
-    // py::class_<
-    //     ripple::TypedField<ripple::STAccount>,
-    //     std::shared_ptr< ripple::TypedField<ripple::STAccount> >,
-    //     ripple::SField
-    // >(m, "TypedFieldSTAccount");
-    //  py::class_<
-    //     ripple::TypedField<ripple::STBlob>,
-    //     std::shared_ptr< ripple::TypedField<ripple::STBlob> >,
-    //     ripple::SField
-    // >(m, "TypedFieldSTBlob");
 
-    // py::class_<
-    //     ripple::OptionaledField<ripple::STAccount>,
-    //     std::shared_ptr< ripple::OptionaledField<ripple::STAccount> >,
-    //     ripple::SField
-    // >(m, "OptionaledFieldSTAccount");
-    //  py::class_<
-    //     ripple::OptionaledField<ripple::STBlob>,
-    //     std::shared_ptr< ripple::OptionaledField<ripple::STBlob> >,
-    //     ripple::SField
-    // >(m, "OptionaledFieldSTBlob");
+    py::class_<ripple::AccountID> AccountID(m, "AccountID");
+    AccountID
+        .def("toBase58",
+            [](const ripple::AccountID &a) {
+                return ripple::toBase58(a);
+            }
+        )
+        .def("__repr__",
+            [](const ripple::AccountID &a) {
+                return "<AccountID:'" + ripple::toBase58(a) + "'>";
+            }
+        );
 
     py::class_<ripple::STObject> STObject(m, "STObject");
     STObject
-        .def(
-            "at",
-            (
-                typename ripple::STAccount::value_type // return type
-                (ripple::STObject::*) // class pointer?
-                (const ripple::TypedField<ripple::STAccount> &) // param type
-                const // const
-            )
-            &ripple::STObject::at, // function name
-            py::arg("f") // arg name
+        .def_property_readonly("Account",
+            [](const ripple::STObject &obj) {
+                return obj[ripple::sfAccount];
+            }
         );
 
     py::class_<ripple::STTx, ripple::STObject> STTx(m, "STTx");
     STTx
         .def("getTxnType", &ripple::STTx::getTxnType);
-    
-    // template<typename T>
-    // void declare_at(std::string &typestr) {
-    //     using TF = ripple::TypedField<T>;
-    //     std::string pyclass_name = std::string("TypedField") + typestr;
-    //     STTx
-    //         .def(
-    //             "at",
-    //             py::overload_cast<TF>(&ripple::STTx::at)
-    //         )
-    // }
-
-    // declare_at<ripple::STAccount>("STAccount");
-    // declare_at<ripple::STBlob>("STBlob");
-    
-//     m.attr("sfAccount") = ripple::sfAccount;
 }
