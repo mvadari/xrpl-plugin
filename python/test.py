227ac9526fb64430f3bd4b8b3150f6029bb64394
Mayukha Vadari <mvadari@ripple.com>
2023-03-15 17:21:49 -0400

    add complete SetRegularKey implementation

--- python/test.py
+++ python/test.py
@@ -1,41 +1,56 @@
 import plugin_transactor
 from plugin_transactor import (
-    TER,
-    NotTEC,
     tesSUCCESS,
     temINVALID_FLAG,
     preflight1,
     preflight2,
     tfUniversalMask,
-    # temBAD_REGKEY,
-    # fixMasterKeyAsRegularKey
+    temBAD_REGKEY,
+    fixMasterKeyAsRegularKey,
+    accountKeylet,
+    signersKeylet,
+    lsfDisableMaster,
+    tecNO_ALTERNATIVE_KEY,
 )
 
 print("IMPORTEDDDD")
+# print(dir(plugin_transactor))
+
 
 def preflight(ctx):
     print("in preflight")
     print(ctx.tx)
     preflight1ret = preflight1(ctx)
-    if preflight1ret != NotTEC(tesSUCCESS):
+    if preflight1ret != tesSUCCESS:
         return preflight1ret
 
     if ctx.tx.getFlags() & tfUniversalMask:
         print("Malformed transaction: Invalid flags set.")
-        return NotTEC(temINVALID_FLAG)
+        return temINVALID_FLAG
 
-    # if (ctx.rules.enabled(fixMasterKeyAsRegularKey) and
-    #     ctx.tx.isFieldPresent(sfRegularKey) and
-    #     (ctx.tx.getAccountID(sfRegularKey) == ctx.tx.getAccountID(sfAccount))):
-    #     return temBAD_REGKEY
+    if ctx.rules.enabled(fixMasterKeyAsRegularKey) and \
+        ctx.tx.isFieldPresent_sfRegularKey() and \
+            ctx.tx.getAccountID_sfRegularKey() == ctx.tx.getAccountID_sfAccount():
+        return temBAD_REGKEY
 
     return preflight2(ctx)
 
 def preclaim(ctx):
     print("in preclaim")
     print(ctx.tx)
-    return TER(tesSUCCESS)
+    return tesSUCCESS
 
 def doApply(ctx):
     print("in doApply")
-    return TER(tesSUCCESS)
+    account = ctx.tx.getAccountID_sfAccount()
+    sle = ctx.view().peek(accountKeylet(account))
+    # skip weird fee stuff
+    if ctx.tx.isFieldPresent_sfRegularKey():
+        sle.setAccountID_sfRegularKey(ctx.tx.getAccountID_sfRegularKey())
+    else:
+        # Account has disabled master key and no multi-signer signer list.
+        if sle.isFlag(lsfDisableMaster) and not ctx.view().peek(signersKeylet(account)):
+            return tecNO_ALTERNATIVE_KEY
+
+        sle.makeFieldAbsent_sfRegularKey()
+    return tesSUCCESS
