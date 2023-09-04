9bcefaba822c2cc9d119cfcad77130d844432fa4
Mayukha Vadari <mvadari@gmail.com>
2023-09-04 11:59:26 +0200

    Bump version to 0.0.5

--- python/setup.py
+++ python/setup.py
@@ -145,7 +145,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="xrpl-plugin",
-    version="0.0.4",
+    version="0.0.5",
     author="Mayukha Vadari",
     author_email="mvadari@ripple.com",
     url="https://github.com/mvadari/xrpl-plugin",
